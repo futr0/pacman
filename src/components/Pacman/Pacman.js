ce8419d1c8fa02df78f70c9465b41f77ae3d2891
artur <artur.itsme@gmail.com>
2020-09-26 11:13:30 +0200

    Pacman component addjustments, icons

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
+import { ReactComponent as PacmanSvg } from './pacman.svg';
+import './style.css';
 
 class Pacman extends Component {
+
+    state = {
+        direction: 'right',
+        position: {
+            top: 0,
+            left: 0
+        }
+    }
+
     render() {
         return( 
-            <div className='pacman'>
-                Hello!
+            <div className='pacman' style={this.state.position}>
+                <PacmanSvg/>
             </div>
         );
     }
+}
+
+Pacman.defaultProps = {
+    step: 50,
+    size: 50, //pacman size 50x50 px
+    // TODO: move to config
+    border: 10 * 2, //2 * border from Border css
+    topScoreBoardHeight: 50
 }
 
 export default Pacman;
