a406429cc3b4ec7dd6c90bf0e3e5076f02b94690
artur <artur.itsme@gmail.com>
2020-09-26 13:10:08 +0200

    Ghost movement part 2

--- src/components/Ghost/Ghost.js
+++ src/components/Ghost/Ghost.js
@@ -13,11 +13,14 @@ class Ghost extends Component {
     }
 
     componentDidMount() {
-        this.changeDirectionInterval = setInterval(this.changeDirection, 2000);
+        const changeInterval = 2000;
+        this.changeDirectionInterval = setInterval(this.changeDirection, changeInterval);
+        this.moveInterval = setInterval(this.move, changeInterval);
     }
 
     componentWillUnmount() {
         clearInterval(this.changeDirectionInterval);
+        clearInterval(this.moveInterval);
     }
 
     changeDirection = () =>  {
@@ -25,8 +28,48 @@ class Ghost extends Component {
         const movement = Math.floor(Math.random() * 4);
 
         this.setState({direction: arrayOfMovement[movement]}, () => {
-            console.log('direction:', this.state.direction)
         });
+    }
+
+    move = () => {
+        //TODO: refactor here and in Pacman component to reuse movement code
+        const currentTop = this.state.position.top;
+        const currentLeft = this.state.position.left;
+        const {direction} = this.state;
+        const {step, border, size, topScoreBoardHeight} = this.props;
+
+        if(direction === 'up') {
+            this.setState({
+                position: {
+                    top: Math.max(currentTop - step, 0),
+                    left: currentLeft
+                }
+            })
+        }
+        else if (direction === 'down') {
+            this.setState({
+                position: {
+                    top: Math.min(currentTop + step, window.innerHeight - border - size - topScoreBoardHeight),
+                    left: currentLeft
+                }
+            })
+        }
+        else if (direction === 'right') {
+            this.setState({
+                position: {
+                    top: currentTop,
+                    left: Math.min(currentLeft + step, window.innerWidth - border - size)
+                }
+            })
+        }
+        else if (direction === 'left') {
+            this.setState({
+                position: {
+                    top: currentTop,
+                    left: Math.max(currentLeft - step, 0)
+                }
+            })
+        }
     }
 
     render() {
