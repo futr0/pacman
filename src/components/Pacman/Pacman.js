30a759ba25e9ebbbbbb3c04a7c5c88c36999b83f
artur <artur.itsme@gmail.com>
2020-09-26 11:53:15 +0200

    Pacman moves

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -26,27 +26,41 @@ class Pacman extends Component {
 
         const currentTop = this.state.position.top;
         const currentLeft = this.state.position.left;
+        const {step} = this.props;
 
         if(event.key === 'ArrowUp') {
             this.setState({
+                position: {
+                    top: currentTop - step,
+                    left: currentLeft
+                },
                 direction: 'up'
             })
-
         }
         else if (event.key === 'ArrowDown') {
             this.setState({
+                position: {
+                    top: currentTop + step,
+                    left: currentLeft
+                },
                 direction: 'down'
             })
         }
-
         else if (event.key === 'ArrowRight') {
             this.setState({
+                position: {
+                    top: currentTop,
+                    left: currentLeft + step
+                },
                 direction: 'right'
             })
         }
-
         else if (event.key === 'ArrowLeft') {
             this.setState({
+                position: {
+                    top: currentTop,
+                    left: currentLeft - step
+                },
                 direction: 'left'
             })
         }
